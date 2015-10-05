e582f40a507267e81cd593e218944c26c5e3c900
Ashok Saravanan M <mani.ashoksaravanan@gmail.com>
2015-10-05 08:31:28 +0530

    Added action for next, previous and replay buttons.

--- app/src/main/java/com/ashoksm/atozforkids/SliderActivity.java
+++ app/src/main/java/com/ashoksm/atozforkids/SliderActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.ashoksm.atozforkids.adapter.SliderPagerAdapter;
@@ -28,7 +31,7 @@ public class SliderActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(itemName);
         }
         final TextView name = (TextView) findViewById(R.id.name);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.slider);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.slider);
 
         switch (itemName) {
             case "Alphabets" :
@@ -55,6 +58,34 @@ public class SliderActivity extends AppCompatActivity {
         });
 
         viewPager.setPageTransformer(true, new DepthPageTransformer());
+
+        final ImageButton replay = (ImageButton) findViewById(R.id.replay);
+        final Button next = (Button) findViewById(R.id.next);
+        final Button previous = (Button) findViewById(R.id.previous);
+        replay.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(0);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+            }
+        });
+
+        previous.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+            }
+        });
+
     }
 
 }
